18e9416652a8de3a5578cac91d6d7c25291a2c46
yngvib <yngvi@ru.is>
2014-09-15 14:44:50 +0000

    sqlhelper and sqladapter

--- src/com/example/ADBTest/DbHelper.java
+++ src/com/example/ADBTest/DbHelper.java
@@ -12,17 +12,32 @@ public class DbHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "STUDENTS_DB";
     public static final int DB_VERSION = 1;
 
+    public static final String TableStudent = "students";
+    public static final String[] TableStudentCols = { "_id", "sid", "name", "cool" };
+
+    private static final String sqlCreateTableStudent =
+            "CREATE TABLE students(" +
+            " _id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            " sid INTEGER NOT NULL," +
+            " name TEXT," +
+            " cool INTEGER " +
+            ");";
+
+    private static final String sqlDropTableStudents =
+            "DROP TABLE IF EXISTS students;";
+
     public DbHelper( Context context ) {
         super( context, DB_NAME, null, DB_VERSION );
-
     }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL( sqlCreateTableStudent );
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL( sqlDropTableStudents );
+        onCreate( db );
     }
 }
--- src/com/example/ADBTest/StudentsAdapter.java
+++ src/com/example/ADBTest/StudentsAdapter.java
@@ -1,7 +1,76 @@
 package com.example.ADBTest;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by yngvi on 15.9.2014.
  */
 public class StudentsAdapter {
+
+    SQLiteDatabase db;
+    DbHelper dbHelper;
+    Context context;
+
+    public StudentsAdapter( Context c ) {
+        context = c;
+    }
+
+    public StudentsAdapter openToRead() {
+        dbHelper = new DbHelper( context );
+        db = dbHelper.getReadableDatabase();
+        return this;
+    }
+
+    public StudentsAdapter openToWrite() {
+        dbHelper = new DbHelper( context );
+        db = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public long insertStudent( int sid, String name, boolean cool ) {
+        String[] cols = DbHelper.TableStudentCols;
+        ContentValues contentValues = new ContentValues();
+        contentValues.put( cols[1], ((Integer)sid).toString() );
+        contentValues.put( cols[2], name );
+        contentValues.put( cols[3], cool ? "1" : "0" );
+        openToWrite();
+        long value = db.insert(DbHelper.TableStudent, null, contentValues );
+        close();
+        return value;
+    }
+
+    public long updateStudent( int sid, String name, boolean cool ) {
+        String[] cols = DbHelper.TableStudentCols;
+        ContentValues contentValues = new ContentValues();
+        contentValues.put( cols[1], ((Integer)sid).toString() );
+        contentValues.put( cols[2], name );
+        contentValues.put( cols[3], cool ? "1" : "0" );
+        openToWrite();
+        long value = db.update(DbHelper.TableStudent, contentValues, cols[1] + sid, null );
+        close();
+        return value;
+    }
+
+    public Cursor queryStudents() {
+        openToRead();
+        Cursor cursor = db.query( DbHelper.TableStudent,
+                                  DbHelper.TableStudentCols, null, null, null, null, null);
+        return cursor;
+    }
+
+    public Cursor queryStudent( int sid) {
+        openToRead();
+        String[] cols = DbHelper.TableStudentCols;
+        Cursor cursor = db.query( DbHelper.TableStudent,
+                cols, cols[1] + "" + sid, null, null, null, null);
+        return cursor;
+    }
+
 }
